bugfix for log scaling z axes

readevents.py
@@ -638,7 +638,7 @@ class App(tk.Frame):
         else:
             x = np.log10(xsign*x_col) if bool(logscale_x) else x_col
             y = np.log10(ysign*y_col) if bool(logscale_y) else y_col
-            z = np.log10(sign*z_col) if bool(logscale_z) else z_col
+            z = np.log10(zsign*z_col) if bool(logscale_z) else z_col
         
         self.stats_f.clf()
         ax = self.stats_f.add_subplot(111, projection = '3d')